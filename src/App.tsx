22f2edfcf8e8b106aeca941a52c16f3f5d3cc606
Adeesha97 <adeeshakawshal1997@gmail.com>
2022-09-08 16:57:04 +0530

    Add all functionalities commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.model";
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos([...todos, { id: Math.random().toString(), text: text }]);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList todos={todos} onDeleteTodo={todoDeleteHandler} />
     </div>
   );
 }
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface TodoListProps {
+  todos: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
+}
+
+const TodoList: React.FC<TodoListProps> = (props) => {
+  return (
+    <ul>
+      {props.todos.map((todo) => (
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          <button onClick={props.onDeleteTodo.bind(null, todo.id)}>
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
